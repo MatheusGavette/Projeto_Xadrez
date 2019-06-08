097258bd4372beab61cf5ccf73e96581daf405cc
MatheusGavette <matheus.gvtt10@gmail.com>
2019-06-08 11:23:20 -0300

    Criação das classes tabuleiro, peca, posicao e do enum cor

---
+++ Projeto_Xadrez/Tabuleiro/Cor.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Projeto_Xadrez.Tabuleiro
+{
+    enum Cor
+    {
+        Branca,
+        Preta,
+        Laranja,
+        Amarela,
+        Azul,
+        Verde,
+        Vermelha,
+        Roxa
+    }
+}
